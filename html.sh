01bec6c85d9140b8cdfa10b7c5308f0a6de5d95b
Tony <nya.tony2010@gmail.com>
2023-12-26 19:41:18 +0100

    chore: add a script to create boilerplate HTML file

---
+++ html.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# This script prompts the shell and
+# creates an empty HTML boilerplate
+
+# Prompt the shell for a filename
+read -rp "Enter a filename: " filename
+
+# Create the filename with the given name
+if touch "$filename"; then
+  # Use curly braces to group the commands and redirect the output
+  {
+    echo "<!DOCTYPE html>"
+    echo "<html lang='en'>"
+    echo "  <head>"
+    echo "    <meta charset='UTF-8'>"
+    echo "    <title>Your Page Title</title>"
+    echo "    <link rel='stylesheet' href='styles.css'>"
+    echo "  </head>"
+    echo "  <body>"
+    echo "  </body>"
+    echo "</html>"
+  } >> "$filename"
+
+  # Print a success message to stdout
+  echo "File $filename created!"
+else
+  # Print an error message if file creation fails
+  echo "Error: Unable to create file $filename."
+fi
